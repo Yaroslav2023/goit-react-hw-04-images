876db473c40e6840a80ebd5d607e51337d8e0e1d
Yaroslav2023 <yishevchenko@ukr.net>
2023-08-18 00:36:20 +0300

    fix-problem

--- src/context/imagesContext.js
+++ src/context/imagesContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect, useMemo } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { getImages } from 'api/search.images';
 
 export const Context = createContext();
